58477141977523b8f5acff73f1f392104959b1d5
irley lopes <irleylopes@gmail.com>
2023-07-08 17:37:01 -0300

    init

---
+++ src/main/java/br/com/irley/studyfc3ci/StudyFc3CiApplication.java
@@ -0,0 +1,13 @@
+package br.com.irley.studyfc3ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFc3CiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyFc3CiApplication.class, args);
+	}
+
+}

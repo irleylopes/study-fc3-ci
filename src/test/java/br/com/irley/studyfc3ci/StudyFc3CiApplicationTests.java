6bce25a4847a3106c9c0dde28171142425b5402a
irley lopes <irleylopes@gmail.com>
2023-07-09 16:10:14 -0300

    remove teste erro ci

--- src/test/java/br/com/irley/studyfc3ci/StudyFc3CiApplicationTests.java
+++ src/test/java/br/com/irley/studyfc3ci/StudyFc3CiApplicationTests.java
@@ -1,6 +1,5 @@
 package br.com.irley.studyfc3ci;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +8,6 @@ class StudyFc3CiApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Assertions.assertEquals(1, 2);
 	}
 
 }
